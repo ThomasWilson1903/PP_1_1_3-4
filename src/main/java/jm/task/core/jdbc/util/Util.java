adac8da64db7318b2d28dc6d714e118dc5adaa4c
thomas <hdnhdxv@gmail.com>
2023-09-19 19:16:49 +0400

    Adding a function

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
     public Connection getConnection() {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/store", "root", "password");) {
+        try (Connection connection = DriverManager.getConnection("127.0.0.1", "test_user", "password")) {
             return connection;
-        } catch (SQLException ex) {
+        } catch (SQLException ignored) {
 
         }
         return null;
