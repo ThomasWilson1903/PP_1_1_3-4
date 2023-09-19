9391eb0889f978798515c6c0a77fe2f70f52a560
thomas <hdnhdxv@gmail.com>
2023-09-19 06:42:49 +0400

    FirstCommit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public Connection getConnection() {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/store", "root", "password");) {
+            return connection;
+        } catch (SQLException ex) {
+
+        }
+        return null;
+
+    }
 }
