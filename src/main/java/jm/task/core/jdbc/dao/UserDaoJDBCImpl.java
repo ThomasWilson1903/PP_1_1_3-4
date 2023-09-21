e167b882b2dc820467c6198a7d91880223361474
thomas <hdnhdxv@gmail.com>
2023-09-21 17:49:31 +0400

    add Functional

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -15,7 +15,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         String sql = "CREATE TABLE `test_table`.`Users` (\n" +
-                "  `idUsers` INT NOT NULL,\n" +
+                "  `idUsers` INT NOT NULL AUTO_INCREMENT ,\n" +
                 "  `Name` VARCHAR(45) NOT NULL,\n" +
                 "  `NameLast` VARCHAR(45) NOT NULL,\n" +
                 "  `Age` INT NOT NULL,\n" +
@@ -31,11 +31,25 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE `test_table`.`Users`;";
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = String.format("INSERT INTO `test_table`.`Users` (`Name`, `NameLast`, `Age`) VALUES ('%s', '%s', '%d');", name, lastName, age);
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
