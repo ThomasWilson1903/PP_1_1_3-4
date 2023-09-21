5f00a7060e3ae1b53625cae7b4c342b8f45478d0
thomas <hdnhdxv@gmail.com>
2023-09-21 17:30:45 +0400

    add Functional

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
@@ -10,11 +12,9 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         Connection s = new Util().getConnection();
-        /*UserDao userDao = new UserDaoJDBCImpl();
-
+        UserDao userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
-
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
+       /* userDao.saveUser("Name1", "LastName1", (byte) 20);
         userDao.saveUser("Name2", "LastName2", (byte) 25);
         userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,6 +14,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        String sql = "CREATE TABLE `test_table`.`Users` (\n" +
+                "  `idUsers` INT NOT NULL,\n" +
+                "  `Name` VARCHAR(45) NOT NULL,\n" +
+                "  `NameLast` VARCHAR(45) NOT NULL,\n" +
+                "  `Age` INT NOT NULL,\n" +
+                "  PRIMARY KEY (`idUsers`));";
+        Connection connection = Util.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
